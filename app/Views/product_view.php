f5553d21e0159c11da697b8c7653163135b3b745
wikupramesthi <wikupb@gmail.com>
2024-01-25 13:00:12 +0700

    add bootsrap

--- app/Views/product_view.php
+++ app/Views/product_view.php
@@ -1,33 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
     <title>Product List</title>
 </head>
+
 <body>
-    <a href="/product/add_new">Add New</a>
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach($product as $row):?>
-            <tr>
-                <td><?= $row['product_name'];?></td>
-                <td><?= $row['product_price'];?></td>
-                <td>
-                    <a href="/product/edit/<?= $row['product_id'];?>">Edit</a>
-                    <a href="/product/delete/<?= $row['product_id'];?>">Delete</a>
-                </td>
-            </tr>
-        <?php endforeach;?>
-        </tbody>
-    </table>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+
+                <?php if (!empty(session()->getFlashdata('message'))) : ?>
+
+                    <div class="alert alert-success">
+                        <?php echo session()->getFlashdata('message'); ?>
+                    </div>
+
+                <?php endif ?>
+
+                <a href="<?php echo base_url('product/add_new') ?>" class="btn btn-md btn-success mb-3">Add New</a>
+
+                <table class="table table-bordered table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($product as $row) : ?>
+                            <tr>
+                                <td><?= $row['product_name']; ?></td>
+                                <td><?= $row['product_price']; ?></td>
+                                <td>
+                                    <a class="btn btn-sm btn-primary" href="/product/edit/<?= $row['product_id']; ?>">Edit</a>
+                                    <a class="btn btn-sm btn-danger" href="/product/delete/<?= $row['product_id']; ?>">Delete</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
