11727bd0eeeb629b7d5694e845b5c69c7f6efdb2
wikupramesthi <wikupb@gmail.com>
2024-01-25 13:24:58 +0700

    edit bootstrap template

--- app/Views/edit_product_view.php
+++ app/Views/edit_product_view.php
@@ -1,17 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
     <title>Edit Product</title>
 </head>
+
 <body>
-    <form action="/product/update" method="post">
-        <input type="text" name="product_name" value="<?= $product->product_name;?>">
-        <input type="text" name="product_price" value="<?= $product->product_price;?>">
-        <input type="hidden" name="product_id" value="<?= $product->product_id;?>">
-        <button type="submit">Update</button>
-    </form>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if (isset($validation)) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $validation->listErrors() ?>
+                    </div>
+                <?php } ?>
+                <div class="card">
+                    <div class="card-body">
+
+                        <form action="/product/update" method="post">
+                            <input type="hidden" name="product_id" value="<?= $product->product_id; ?>">
+
+                            <div class="form-group">
+                                <label>Product Name</label>
+                                <input type="text" name="product_name" value="<?= $product->product_name; ?>">
+                            </div>
+                            <div class="form-group">
+                                <label>Product Price</label>
+                                <input type="text" name="product_price" value="<?= $product->product_price; ?>">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
